8d4bf48cc96332422b93458461d465c3848a9bfa
Alexander Snitko <asyncamisu@gmail.com>
2022-05-03 22:53:49 +0300

    Initial Commit

---
+++ MMColorize/MMColorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMColorizeApp.swift
+//  MMColorize
+//
+//  Created by Alexander Snitko on 3.05.22.
+//
+
+import SwiftUI
+
+@main
+struct MMColorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
