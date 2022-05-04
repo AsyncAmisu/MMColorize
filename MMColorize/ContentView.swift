8c8896d4221e1803c7130a2cbb53b5769e5d7d7b
Alexander Snitko <asyncamisu@gmail.com>
2022-05-05 01:52:06 +0300

    slider button and game object added

--- MMColorize/ContentView.swift
+++ MMColorize/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var game = Game()
+    @State var guess: RGB
+    var target = RGB.random()
+    
     var body: some View {
         VStack {
-            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Color(rgbStruct: game.target)
             Text("R: ??? G: ??? B: ???")
                 .padding()
+            Color(rgbStruct: guess)
+            Text("R: 204 G: 76 B: 178")
+                .padding()
+            Slider(value: .constant(0.5))
+            Button("Hit me!") {
+                    
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(guess: RGB(red: 0.8, green: 0.3, blue: 0.7))
     }
 }
---
+++ MMColorize/Model/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  MMColorize
+//
+//  Created by Alexander Snitko on 3.05.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  /// Create a Color view from an RGB object.
+  ///   - parameters:
+  ///     - rgb: The RGB object.
+  init(rgbStruct rgb: RGB) {
+    self.init(red: rgb.red, green: rgb.green, blue: rgb.blue)
+  }
+}
