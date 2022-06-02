37fc0509d9b85558ee276ecfca23b11a80141892
Alexander Snitko <asyncamisu@gmail.com>
2022-06-03 01:07:37 +0300

    softfix

--- MMColorize/ContentView.swift
+++ MMColorize/ContentView.swift
@@ -12,46 +12,53 @@ struct ContentView: View {
     @State var game = Game()
     @State var guess: RGB
     @State var showScore: Bool = false
+    let circleSize: CGFloat = 0.275
+    let labelHeight: CGFloat = 0.06
+    let labelWidth: CGFloat = 0.53
+    let buttonWidth: CGFloat = 0.7
 //    var target = RGB.random()
     
     var body: some View {
-        ZStack {
-            Color.element
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                ColorCircle(rgb: game.target, size: 200)
-                if !showScore {
-                    BevelText(text: "R: ??? G: ??? B: ???", width: 200, height: 48)
-                } else {
-                    BevelText(text: game.target.intString(), width: 200, height: 48)
+        GeometryReader { proxy in
+            ZStack {
+                Color.element
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    ColorCircle(rgb: game.target, size: proxy.size.height * circleSize)
+                    if !showScore {
+                        BevelText(text: "R: ??? G: ??? B: ???", width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                    } else {
+                        BevelText(text: game.target.intString(), width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                    }
+                    ColorCircle(rgb: guess, size: proxy.size.height * circleSize)
+                    BevelText(text: guess.intString(), width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                    ColorSlider(value: $guess.red, trackColor: .red)
+                    ColorSlider(value: $guess.green, trackColor: .green)
+                    ColorSlider(value: $guess.blue, trackColor: .blue)
+                    Button {
+                        game.check(guess: guess)
+                        showScore = true
+                    } label: {
+                        Text("Hit me!")
+                    }
+                    .buttonStyle(NeuButtonStyle(width: proxy.size.width * buttonWidth, height: proxy.size.height * labelHeight))
+                    .alert(isPresented: $showScore) {
+                        Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)), dismissButton: .default(Text("OK")) {
+                            game.startNewRound()
+                            guess = RGB()
+                        })
+                    }
                 }
-                ColorCircle(rgb: guess, size: 200)
-                BevelText(text: guess.intString(), width: 200, height: 48)
-                ColorSlider(value: $guess.red, trackColor: .red)
-                ColorSlider(value: $guess.green, trackColor: .green)
-                ColorSlider(value: $guess.blue, trackColor: .blue)
-                Button {
-                    game.check(guess: guess)
-                    showScore = true
-                } label: {
-                    Text("Hit me!")
-                }
-                .buttonStyle(NeuButtonStyle(width: 327, height: 48))
-                .alert(isPresented: $showScore) {
-                    Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)), dismissButton: .default(Text("OK")) {
-                        game.startNewRound()
-                        guess = RGB()
-                    })
-                }
+                .font(.headline)
             }
-            .font(.headline)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(guess: RGB())
+            ContentView(guess: RGB())
+                .previewDevice("iPhone 11")
     }
 }
 
